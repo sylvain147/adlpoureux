b05298f8c52141051f2fe0eacf8f5fd0758924a2
sylvain <sylvain.attoumani@gmail.com>
2020-04-22 16:16:20 +0200

    graphql

--- graphql.js
+++ graphql.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server');
+const {ApolloServer, gql} = require('apollo-server');
 const mysql = require('promise-mysql');
 let params = {
     host: `localhost`,
@@ -6,7 +6,6 @@ let params = {
     password: `Zg4nwp6jim`,
     database: `adl`,
 };
-console.log(params)
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -15,14 +14,20 @@ type Mutation {
   createProduct(productInput: ProductInput!): Product!
   createUser(userInput: UserInput!): User!
 }
-
 type Query {
-  products : [Product]!
-  product(id: Int!): Product!
-  user(id: Int!): User!
+  getProducts : [Product]!
+  getProduct(id: Int!): Product!
+  getUser(id: Int!): User!
+  getUsers : [User]!
+}
+type Level  {
+  level_id : Int!
+  name : String!
+  level : Int!
 }
 type User {
   user_id : Int!
+  avatar : String!
   firstname : String
   lastname : String
   username : String!
@@ -43,6 +48,52 @@ type Product {
   created_at : String!
   deleted_at : String
 }
+
+type Picture_product {
+  picture_product_id : Int!
+  product_id : Int!
+  product : Product
+  name : String
+  url : String!
+  created_at : String!
+  deleted_at : String
+  
+}
+
+type Category {
+  category_id : Int!
+  title : String!
+  description : String!
+  slug : String!
+  created_at : String!
+  deleted_at : String
+}
+
+type Tag {
+  tag_id : Int!
+  owner_id : Int!
+  title : String!
+  slug : String!
+  created_at : String!
+  deleted_at : String
+}
+
+type Order {
+  order_id : Int!
+  user_id : Int!
+  user : User!
+}
+
+type Order_line {
+  order_line_id : Int!
+  order_id : Int!
+  order : Order
+  product_id : Int!
+  product : Product
+  quantity : Int!
+  price : Int!
+  total_price : Int!
+}
 input ProductInput {
   title : String
   description : String
@@ -60,40 +111,57 @@ input UserInput {
 }
 `
 const resolvers = {
-  Query: {
-    async product(_,{id},context) {
-      const product = await context.connection.query(`SELECT * from product where product_id = ?`, [id]) 
-      return product[0]
+    Query: {
+        async getProduct(_, {id}, context) {
+            const product = await context.connection.query(`SELECT * from product where product_id = ?`, [id])
+            return product[0]
+        },
+        async getProducts(_, {}, context) {
+            const products = await context.connection.query(`SELECT * from product`)
+            return products;
+        },
+        async getUser(_, {id}, context) {
+            const user = await context.connection.query(`SELECT * from user where user_id = ?`, [id])
+            return user;
+        },
+        async getUsers(_,{},context) {
+            const users = await context.connection.query(`SELECT * from user`, [id])
+            return users;
+        }
     },
-    async products(_,{},context) {
-      const products = await context.connection.query(`SELECT * from product`)
-      return products;
-    }
-  },
-  Mutation: {
-    async createProduct(){
+    Mutation: {
+        async createProduct(_, {productInput}, context) {
+            const {insertId} = await context.connection.query('INSERT INTO product set ?', {
+                title : productInput.title,
+                description :  productInput.description,
+                price :  productInput.price,
+                user_id : productInput.user_id,
+                slug :  productInput.slug,
+                created_at :  new Date(),
+            })
+            const product = await context.connection.query(`SELECT * from product where product_id = ?`, insertId)
+            return product[0]
+        },
+        async createUser(_, {userInput}, context) {
+            const {insertId} = await context.connection.query('INSERT INTO user set ?', {
+                firstname: userInput.firstname,
+                lastname: userInput.lastname,
+                username: userInput.username,
+                password: userInput.password,
+                email: userInput.email,
+                created_at: new Date()
+            })
+            const user = await context.connection.query(`SELECT * from user where user_id = ?`, [insertId])
 
+            return user[0]
+        }
     },
-    async createUser(_,{userInput},context){
-      const {insertId} = await context.connection.query('INSERT INTO user set ?', {
-        firstname : userInput.firstname,
-        lastname : userInput.lastname,
-        username : userInput.username, 
-        password : userInput.password, 
-        email : userInput.email,
-        created_at: new Date()
-      })
-      const user = await context.connection.query(`SELECT * from user where user_id = ?`, [insertId])
-      console.log(user)
-      return user[0]
+    Product: {
+        async user(parent, {}, context) {
+            const user = await context.connection.query(`SELECT * from user where user_id = ?`, [parent.user_id])
+            return user[0]
+        }
     }
-  },
-  Product:{
-    async user(parent,{},context) {
-      const user = await context.connection.query(`SELECT * from user where user_id = ?`, [parent.user_id])
-      return user[0]
-    }  
-  }
 }
 let connection
 const getConnection = async () => {
@@ -105,17 +173,17 @@ const getConnection = async () => {
 
     return connection
 };
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers,
-  async context() {
-    return {
-      connection : await getConnection()
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    async context() {
+        return {
+            connection: await getConnection()
+        }
     }
-  } 
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen().then(({url}) => {
+    console.log(`🚀  Server ready at ${url}`);
 })
